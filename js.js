1f53feb9bd32d36dcaa22a2b6f7505a3c67fe618
Froods <34389217+Froods@users.noreply.github.com>
2025-03-09 22:09:51 +0100

    Add pencil size feature
    Added a button which makes it possible for users to choose a pencil size.

--- js.js
+++ js.js
@@ -1,22 +1,49 @@
 const container = document.querySelector("#container");
+const btn = document.querySelector("#btn");
 
 let nSquares = 16;
-let squareWidth = (1/nSquares * 100).toFixed(2) + "%";
-console.log(squareWidth);
 
-for (let i = 0; i < nSquares; i++) {
+function setSquares() {
+    nSquares = prompt("Number of squares per row: \nChoose from 0 - 100.","")
+}
 
-    for (let j = 0; j < nSquares; j++) {
-        const gridSquare = document.createElement("div");
-        gridSquare.setAttribute("style", `width: ${squareWidth}; padding-top: ${squareWidth}`);
-        gridSquare.setAttribute("class", "gridSquare")
-        container.appendChild(gridSquare);
+function drawSquares() {
+
+    let squareWidth = (1/nSquares * 100).toFixed(2) + "%";
+
+    for (let i = 0; i < nSquares; i++) {
+
+        for (let j = 0; j < nSquares; j++) {
+            const gridSquare = document.createElement("div");
+            gridSquare.setAttribute("style", `width: ${squareWidth}; padding-top: ${squareWidth}`);
+            gridSquare.setAttribute("class", "gridSquare")
+            container.appendChild(gridSquare);
+        }
+
     }
 
 }
+
+function deleteSquares() {
+
+    while (document.querySelector(".gridSquare")) {
+        document.querySelector(".gridSquare").remove();
+    }
+
+}
+
+drawSquares();
 
 container.addEventListener('mouseover', function(event) {
     if (event.target && event.target.classList.contains('gridSquare')) {
         event.target.style.backgroundColor = "black";
     }
+});
+
+btn.addEventListener("click", () => {
+    do {
+        setSquares();
+    } while (nSquares <= 0 || nSquares > 100)
+        deleteSquares();
+        drawSquares();
 });
