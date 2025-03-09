5ea88fbb374fefb835007e2481fbac4e63b8f441
Froods <34389217+Froods@users.noreply.github.com>
2025-03-09 19:56:49 +0100

    Add drawing feature.
    Made grid squares change color when moused over.

--- js.js
+++ js.js
@@ -1,12 +1,22 @@
 const container = document.querySelector("#container");
 
-for (let i = 0; i < 16; i++) {
+let nSquares = 16;
+let squareWidth = (1/nSquares * 100).toFixed(2) + "%";
+console.log(squareWidth);
 
-    for (let j = 0; j < 16; j++) {
+for (let i = 0; i < nSquares; i++) {
+
+    for (let j = 0; j < nSquares; j++) {
         const gridSquare = document.createElement("div");
+        gridSquare.setAttribute("style", `width: ${squareWidth}; padding-top: ${squareWidth}`);
         gridSquare.setAttribute("class", "gridSquare")
         container.appendChild(gridSquare);
     }
 
-
 }
+
+container.addEventListener('mouseover', function(event) {
+    if (event.target && event.target.classList.contains('gridSquare')) {
+        event.target.style.backgroundColor = "black";
+    }
+});
